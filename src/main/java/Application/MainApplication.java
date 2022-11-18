b806d171692cc7b2027f3722c8d5f6c1da492553
Anastasiia Petrych <a.petrych@gmail.com>
2022-11-18 11:26:52 +0100

    Added time measurement for scraping

--- src/main/java/Application/MainApplication.java
+++ src/main/java/Application/MainApplication.java
@@ -1,14 +1,32 @@
 package Application;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class MainApplication {
-
+    
     public static void main(String[] args) {
-
-
-        System.out.println("Starting here");
-
-        WebCrawler crawler = new WebCrawler();
-
+        
+        WebCrawlerNotOptimized crawler = new WebCrawlerNotOptimized();
+        
+        LocalTime startTime = LocalTime.now();
+        System.out.println("Started scraping");
+        
         crawler.getPageLinks("http://books.toscrape.com/");
+        
+        LocalTime endTime = LocalTime.now();
+        System.out.println("Finished scraping after " + getDurationAsString(startTime, endTime));
     }
+    
+    private static String getDurationAsString(LocalTime startTime, LocalTime endTime) {
+        
+        Duration duration = Duration.between(startTime, endTime);
+        
+        long h = duration.toHours();
+        long min = duration.toMinutesPart();
+        long sec = duration.toSecondsPart();
+        
+        return String.format("%02d h %02d min %02d sec", h, min, sec);
+    }
+    
 }
