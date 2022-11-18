810550cab1782e9df1b06947f02d4bb9e03bdeae
Anastasiia Petrych <a.petrych@gmail.com>
2022-11-18 11:28:05 +0100

    Added interfaces for a crawler and a scraper for later to separate their different implementations

--- src/main/java/Application/WebCrawler.java
+++ src/main/java/Application/WebCrawler.java
@@ -1,68 +1,8 @@
 package Application;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.Set;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashSet;
-
-public class WebCrawler {
-    private HashSet<String> urlLinks;
-
-    public WebCrawler() {
-        urlLinks = new HashSet<String>();
-
-        File file = new File("ScrapedWebsite");
-        file.mkdirs();
-        System.out.println("Created ScrapedWebsite folder, will now commence scraping!");
-        System.out.println("-");
-    }
-
-    public void getPageLinks(String URL) {
-
-        String substringLink = URL.substring(26);
-
-        System.out.println("-");
-        System.out.println("-Crawler crawling for new href-");
-
-
-        if (!urlLinks.contains(substringLink)) {
-            System.out.println("Crawler found: " + substringLink);
-            try {
-                if (urlLinks.add(substringLink)) {
-                    System.out.println(substringLink);
-                    WebScraper.scrape(substringLink);
-                }
-
-                Document doc = Jsoup.connect(URL).ignoreContentType(true).get();
-                Elements availableImgsOnPage = doc.select("img[src]");
-                Elements availableCssOnPage = doc.select("link[href]");
-                Elements availableScriptOnPage = doc.select("script[src]");
-                Elements availableLinksOnPage = doc.select("a[href]");
-                for (Element ele1 : availableImgsOnPage) {
-                    getPageLinks(ele1.attr("abs:src"));
-                    for (Element ele2 : availableCssOnPage) {
-                        getPageLinks(ele2.attr("abs:href"));
-                        for (Element ele3 : availableScriptOnPage) {
-                            getPageLinks(ele3.attr("abs:src"));
-                            for (Element ele4 : availableLinksOnPage) {
-                                getPageLinks(ele4.attr("abs:href"));
-                            }
-                        }
-                    }
-                }
-
-            } catch (IOException e) {
-                System.err.println("For '" + URL + "': " + e.getMessage());
-            }
-        } else {
-            System.out.println("Crawler ignored duplicate.");
-
-        }
-    }
+public interface WebCrawler {
+	
+	Set<String> findAllURLs(String initialURL);
 }
-
-
---
+++ src/main/java/Application/WebScraperNotOptimized.java
@@ -0,0 +1,96 @@
+package Application;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.io.IOException;
+
+
+public class WebScraperNotOptimized {
+    private static String filteredDirectory = "";
+    private static File fileToBeWritten;
+    private static File imgToBeWritten;
+
+    public static void successMessage(String created){
+        String message = String.format("Created: '%s'", created);
+        System.out.println(message);
+    }
+
+    public static void scrape(String pathFromCrawler) {
+
+
+        try {
+            // Create URL object
+            URL url = new URL("http://books.toscrape.com/" + pathFromCrawler);
+
+            String filteredFileName = saveDirectory(pathFromCrawler);
+
+            if (filteredFileName.endsWith(".jpg")) {
+                saveJpg(url, filteredFileName);
+            } else {
+                saveFile(url, filteredFileName);
+            }
+        }
+        // Exceptions
+        catch (MalformedURLException mue) {
+            System.out.println("Malformed URL Exception raised");
+        } catch (IOException ie) {
+            System.out.println("External library, skipping.");
+        }
+    }
+
+    private static String saveDirectory(String pathFromCrawler) {
+        int indexToFilterDirectory = pathFromCrawler.lastIndexOf('/');
+        int indexToFilterFileName = pathFromCrawler.length();
+
+        String filteredFileName = pathFromCrawler.substring(indexToFilterDirectory + 1, indexToFilterFileName);
+
+        try {
+            filteredDirectory = pathFromCrawler.substring(0, indexToFilterDirectory);
+            File file = new File("ScrapedWebsite", filteredDirectory);
+            file.mkdirs();
+            successMessage(filteredDirectory);
+        } catch (StringIndexOutOfBoundsException sioobe) {
+            //If there's no directory
+            System.out.println("No directory needed.");
+        }
+        return filteredFileName;
+    }
+
+    private static void saveJpg(URL url, String filteredFileName) throws IOException {
+        if (filteredDirectory.equals("")) {
+            imgToBeWritten = new File("ScrapedWebsite", filteredFileName);
+        } else {
+            imgToBeWritten = new File("ScrapedWebsite/" + filteredDirectory, filteredFileName);
+        }
+        BufferedImage image =null;
+        image = ImageIO.read(url);
+        ImageIO.write(image, "jpg",new File(String.valueOf(imgToBeWritten)));
+        successMessage(filteredFileName);
+    }
+
+    private static void saveFile(URL url, String filteredFileName) throws IOException {
+        if (filteredDirectory.equals("")) {
+            fileToBeWritten = new File("ScrapedWebsite", filteredFileName);
+        } else {
+            fileToBeWritten = new File("ScrapedWebsite/" + filteredDirectory, filteredFileName);
+        }
+        BufferedWriter writer =
+                new BufferedWriter(new FileWriter(fileToBeWritten));
+
+        BufferedReader readr =
+                new BufferedReader(new InputStreamReader(url.openStream()));
+
+        // read each line from stream till end
+        String line;
+        while ((line = readr.readLine()) != null) {
+            writer.write(line);
+        }
+        successMessage(filteredFileName);
+        readr.close();
+        writer.close();
+    }
+}
